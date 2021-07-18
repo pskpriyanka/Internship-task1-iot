9031b56dc51e667cbdd0808790889453e5acfeb6
pskpriyanka <62655713+pskpriyanka@users.noreply.github.com>
2021-07-18 17:23:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+#TASK1 OF IOT DOMAIN
+# OBJECT DETECTION
+# AUTHOR : PRIYANKA SUNIL KIZHEKETHOTTAM
+
+import cv2
+import numpy as np
+
+# for traing use lena.png
+#img = cv2.imread('lena.PNG')
+#settings
+thres =0.5 #threshold to detect object
+nms_threshold = 0.2
+cap= cv2.VideoCapture(1)
+cap.set(3,640)
+cap.set(4,480)
+cap.set(10,150)
+
+
+classNames= []
+classFile = 'coco.names'
+with open(classFile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+#sending valuesand set parameters
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5,127.5,127.5))
+net.setInputSwapRB(True)
+
+# needs to be in loop
+while True:
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    bbox = list(bbox)
+    confs = list(np.array(confs).reshape(1,-1)[0])
+    confs = list(map(float,confs))
+
+
+
+    indices = cv2.dnn.NMSBoxes(bbox,confs,thres,nms_threshold)
+    print(indices)
+
+    for i in indices:
+        i = i[0]
+        box =bbox[i]
+        x,y,w,h =box[0],box[1],box[2],box[3]
+        cv2.rectangle(img, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
+        cv2.putText(img, classNames[classIds[i][0]-1].upper(), (box[0] + 10, box[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
+
+
